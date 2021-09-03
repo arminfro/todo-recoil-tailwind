6eef1ed206e7bc6b50b63e9ee1d9246b81a50b95
Armin Fröhlich <mail@arminfroehlich.de>
2021-09-03 13:34:41 +0200

    add src/test/utils.tsx and ts-jest

---
+++ src/test/utils.tsx
@@ -0,0 +1,62 @@
+import {render, RenderOptions, RenderResult} from '@testing-library/react';
+import axios, {AxiosPromise, AxiosResponse} from 'axios';
+import {ReactElement, ReactNode} from 'react';
+import AppContext from 'src/components/AppContext';
+import {mocked} from 'ts-jest/utils';
+
+const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'queries'> & {route?: string}): RenderResult => {
+  if (options?.route) {
+    window.history.pushState({}, 'document.title', options.route)
+  }
+
+  const providers = ({children}: {children?: ReactNode}) => {
+    return (
+      <AppContext>
+        <>{children}</>
+      </AppContext>
+    )
+  }
+
+  return render(ui, {
+    wrapper: providers,
+    ...options
+  })
+}
+
+const matchSnapshot = (renderResult: RenderResult): void => {
+  const {baseElement} = renderResult;
+  expect(baseElement).toMatchSnapshot();
+}
+
+jest.mock('axios');
+const mockAxios = (): void => {
+  const axiosResponse: AxiosResponse = {
+    status: 200,
+    statusText: '',
+    headers: null,
+    config: {},
+    data: {test: 'ok'}
+  }
+  mocked(axios).mockImplementation((url, config): AxiosPromise => {
+    console.log('url', url, 'config', config);
+    if (config) {
+      switch (config.method) {
+      case 'get':
+        if (url) {
+          return Promise.resolve({
+            ...axiosResponse,
+          })
+        }
+        break;
+      }
+    }
+    return Promise.resolve(axiosResponse)
+  })
+}
+
+// re-export everything
+export * from '@testing-library/react';
+
+// override render method
+export {customRender as render, matchSnapshot, mockAxios};
+
