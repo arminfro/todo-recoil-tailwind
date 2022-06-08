54693a4f8f91047e91cfca38ccb211b05b9948ce
Armin Fröhlich <mail@arminfroehlich.de>
2022-06-08 14:05:16 +0200

    refactor: update LoadingSpinner

--- src/components/utils/LoadingSpinner.tsx
+++ src/components/utils/LoadingSpinner.tsx
@@ -3,13 +3,15 @@ import { ReactElement, useMemo } from 'react';
 export default function LoadingSpinner(): ReactElement {
   const style = useMemo(() => ({ borderTopColor: 'transparent' }), []);
 
+  console.log('LoadingSpinner!!');
+
   return (
-    <div>
+    <div className="py-4 mt-32 h-34">
       <div
         style={style}
-        className="w-32 h-32 mx-auto mt-32 border-4 border-indigo-700 border-dotted rounded-full dark:border-indigo-400 animate-spin"
+        className="w-16 h-16 mx-auto border-2 border-indigo-700 border-dotted rounded-full dark:border-indigo-400 animate-spin"
       />
-      <div className="relative mx-auto font-light text-center bg-transparent -top-16 dark:text-indigo-200">
+      <div className="pt-1 relative font-light text-center bg-transparent dark:text-indigo-200">
         Loading
       </div>
     </div>
